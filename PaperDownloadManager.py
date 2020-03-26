9f3f495ffab260daffcd95ed84036a8312c43bbb
unknown <blakempjones@gmail.com>
2020-03-26 16:46:55 -0400

    Adds function descriptions

--- PaperDownloadManager.py
+++ PaperDownloadManager.py
@@ -11,14 +11,34 @@ from watchdog.events import PatternMatchingEventHandler
 from watchdog import events as evt
 # For pdf reading
 from PyPDF2 import PdfFileReader
-# For pop up dialog
+# For pop up dialog (Windows only)
 import ctypes
 # For file management
 from pathlib import Path
 from os import rename
 
-def GetCitation(title: str, formatStr: str, bibFolder: str):
-       
+def GetCitation(title: str, formatStr: str, bibFolder: str) -> str:
+    '''
+    Given the title of an article use the package Scholarly to query google 
+    scholar for that article and return the reference file in the format 
+    specified by the format string (currently only .ris is implemented). Write 
+    the ris file to the bibFolder location.
+
+    Parameters
+    ----------
+    title : str
+        Title of the article.
+    formatStr : str
+        Identifier of the filetype for the reference (only ris is implemented).
+    bibFolder : str
+        Folder to write the reference file to.
+
+    Returns
+    -------
+    str
+        Returns the text of the reference file.
+
+    '''
     # Currently only supports reference manager format
     formats = {"rm": ".ris"}
 
@@ -50,12 +70,36 @@ def GetCitation(title: str, formatStr: str, bibFolder: str):
     return citationText
 
 
-def dialogBox(msg):
+def dialogBox(msg: str) -> None:
+    '''
+    Opens a Windows dialog box and displays msg.
+
+    Parameters
+    ----------
+    msg : str
+        Text to display.
+
+    '''
     box = ctypes.windll.user32.MessageBoxW
     box(None, msg, "Paper Download Manager", 0)
     
 
-def HandleNewFile(event):
+def HandleNewFile(event: evt.FileCreatedEvent) -> None:
+    '''
+    Event handler for a new file in the watched folder. Attempts to get the 
+    title from the metadata. If no title is found it uses the filename. The 
+    title is used to query Google Scholar and download the reference file. If 
+    sucessful, the reference file is written to the bibFolder and the citation 
+    is displayed in a dialog box. If the paper cannot be found, the user is 
+    notified by a dialog box.
+
+    Parameters
+    ----------
+    event : evt.FileCreatedEvent
+        Watchdog filesystem event.
+
+
+    '''
     print(event)
     # Filter out any temp download files
     if (event.src_path.split(".")[-1] == "crdownload"):
@@ -79,7 +123,19 @@ def HandleNewFile(event):
         dialogBox("No citation found for: " + event.src_path)
     
     
-def HandleRenameFile(event):
+def HandleRenameFile(event: evt.FileMovedEvent) -> None:
+    '''
+    Event handler for a renamed file in the watched folder. Checks to see
+    if the new name is the name of an existing reference file, if not it raises
+    a new file event which is caught by HandleNewFile.
+
+    Parameters
+    ----------
+    event : evt.FileMovedEvent
+        DESCRIPTION.
+
+
+    '''
     print(event)
     # Filter out any temp download files
     if (event.src_path.split(".")[-1] == "crdownload"):
@@ -100,8 +156,23 @@ def HandleRenameFile(event):
         rename(event.dest_path, newTitle)
         
  
-def ExtractTitle(fullFileName):
-    
+def ExtractTitle(fullFileName: str) -> str:
+    '''
+    Given a filepath to an article pdf, opens the pdf and reads the 
+    metadata for a title. If none found it uses the filename of the pdf as the 
+    title for the paper.
+
+    Parameters
+    ----------
+    fullFileName : str
+        Path to an article pdf.
+
+    Returns
+    -------
+    str
+        The title as found in the metadata or the filename of the pdf.
+
+    '''
     title = ""
     # Extracts the meta data from the pdf
     with open(fullFileName, 'rb') as f:
@@ -121,21 +192,25 @@ def ExtractTitle(fullFileName):
     title = re.sub(forbiddenChars,'', title)
     return title
     
+
 if __name__ == "__main__":
     
     if (len(sys.argv) != 3):
-        
-        raise EOFError("Paths to the folder to watch and biblipgraphy folder are required")
+        raise EOFError("Paths to the folder to watch and biblipgraphy folder are required." +
+            " Call this file by: \n PaperDownloadManager.py" + 
+            " path_to_folder_to_watch path_to_bibliography_folder")
           
     folderName = sys.argv[1]
     
     global bibFolder 
     bibFolder = sys.argv[2]
     
+    # Set event handlers
     event_handler = PatternMatchingEventHandler(patterns = folderName + "*");
     event_handler.on_created = HandleNewFile;
     event_handler.on_moved = HandleRenameFile
-
+    
+    # Run watchdog observer for the specified folders
     observer = Observer()
     observer.schedule(event_handler, folderName)
     observer.start()
